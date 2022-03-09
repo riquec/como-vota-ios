ea45dec10b39189f0a334a3ed911f9f9079319bd
riquec <henriqueeejc@gmail.com>
2022-03-09 07:55:53 -0300

    Initial Commit

---
+++ Shared/Como_VotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Como_VotaApp.swift
+//  Shared
+//
+//  Created by Henrique Cortinovis on 09/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Como_VotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
