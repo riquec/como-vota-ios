22bf4c64382b44b781d79be9073ddcfb6b5c394c
riquec <henriqueeejc@gmail.com>
2022-03-23 10:14:24 -0300

    adiciona swipe para login

---
+++ Common/Constants/Design/Size/DotIndicatorSize.swift
@@ -0,0 +1,16 @@
+//
+//  DotIndicatorSize.swift
+//  Como Vota (iOS)
+//
+//  Created by Henrique Cortinovis on 23/03/22.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct DotIndicatorSize {
+    
+    // Title
+    static let DOT_SIZE = CGFloat(10)
+}
--- Login/LoginView.swift
+++ Login/LoginView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    private static let EMAIL_STEP = "EMAIL"
+    private static let PASSWORD_STEP = "PASSWORD"
+    
+    private let steps: [String] = [EMAIL_STEP, PASSWORD_STEP]
+    
+    init() {
+        UIPageControl.appearance().currentPageIndicatorTintColor = .blue
+                UIPageControl.appearance().pageIndicatorTintColor = .red
+                UIPageControl.appearance().tintColor = .red
+    }
+ 
     var body: some View {
-        Text("Eu sou login")
+        ScrollView(.init()) {
+            TabView {
+                ForEach(steps.indices, id: \.self) { index in
+                    let selectedStep = steps[index]
+                    
+                    if (selectedStep == LoginView.EMAIL_STEP) {
+                        LoginEmailView()
+                    } else {
+                        LoginPasswordView()
+                    }
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .overlay(dots, alignment: .top)
+        }
+    }
+    
+    private var dots: some View {
+        HStack(spacing: 15) {
+            ForEach(steps.indices, id: \.self) { index in
+                Capsule()
+                    .fill(Color.black)
+                    .frame(width: DotIndicatorSize.DOT_SIZE, height: DotIndicatorSize.DOT_SIZE)
+                    .padding(.top, Spacing.SPACING_10)
+                
+            }
+        }
+    
     }
 }
 
